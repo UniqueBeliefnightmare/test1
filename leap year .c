080a64a7f43036e9aeb31cfe00aa7824059b09dd
UniqueBeliefnightmare <1871937346@qq.com>
2019-04-19 20:26:09 +0800

    leap year

---
+++ leap year .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int year = 0;
+	for (year = 1000; year <= 2000; year++){
+		if (year % 4 == 0 && year%100!=0 || year % 400 == 0) {
+			printf("%d\n", year);
+		}
+	}
+	system("pause");
+	return 0;
+}
